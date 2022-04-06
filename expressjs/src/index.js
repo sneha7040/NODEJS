73d5343f64cb6d6552332cd0735e77bdee94a657
sneha7040 <snehamondal7040@gmail.com>
2022-04-06 13:21:45 +0530

    html css javascript static

--- expressjs/src/index.js
+++ expressjs/src/index.js
@@ -1,7 +1,11 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
 const port = 3000;
+
+const staticPath=path.join(__dirname,"../public");
+app.use(express.static(staticPath));  //built in middleware
 // app.get(route,callback);
 app.get("/", (req, res) => {
   res.status(200).write("<h1>this is my home page</h1>");
