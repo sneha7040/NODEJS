72e5eeb566e8beb970f04b44c7a2142955f15f7c
sneha7040 <snehamondal7040@gmail.com>
2022-04-06 12:34:40 +0530

    express routing

--- expressjs/app.js
+++ expressjs/app.js
@@ -1,10 +1,27 @@
 const express = require("express");
 
 const app = express();
+const port = 3000;
 // app.get(route,callback);
-app.get("/", (res, req)=>{
-    res.send("hello world");
+app.get("/", (req, res)=>{
+    res.status(200).send("this is my home page");
+});  
+app.get("/about",(req, res)=>{
+    res.status(200).send("this is about page");
 });
-app.listen(8000,()=>{
-    console.log("listen the port 8000");
-})
+
+app.get("/contact",(req,res)=>{
+    res.status(200).send("this is my contact page");
+});
+
+
+app.listen(port,()=>{
+    console.log(`listen the port ${port}`);
+});   
+
+
+
+
+
+
+
