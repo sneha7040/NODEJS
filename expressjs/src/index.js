ecd711eb810d42e4804f41ea205b95aa1e64de5b
sneha7040 <snehamondal7040@gmail.com>
2022-04-08 13:59:11 +0530

    partial,template engine,views

--- expressjs/src/index.js
+++ expressjs/src/index.js
@@ -1,19 +1,36 @@
 const path = require("path");
 const express = require("express");
+// const res = require("express/lib/response");
+const hbs = require("hbs");
 
 const app = express();
 const port = 3000;
 
-const staticPath=path.join(__dirname,"../public");
+const staticPath = path.join(__dirname, "../public");
+const templatepath = path.join(__dirname,"../templates/views")
+const partialpath = path.join(__dirname,"../templates/partial")
+//set the view engine
+app.set("view engine", "hbs");
+app.set("views",templatepath);
+hbs.registerPartials(partialpath);
 app.use(express.static(staticPath));  //built in middleware
 // app.get(route,callback);
+
+//templete engine route
+app.get("", (req, res) => {
+  res.render("index", {
+    sneha: "welcome",
+  });
+});
+
+
 app.get("/", (req, res) => {
   res.status(200).write("<h1>this is my home page</h1>");
   res.status(200).write("<h1>this is my againhome page</h1>");
   res.send();
 });
 app.get("/about", (req, res) => {
-  res.status(200).send("this is about page");
+  res.render("about");
 });
 
 app.get("/contact", (req, res) => {
@@ -31,70 +48,68 @@ app.get("/temp", (req, res) => {
     },
 
     {
-        id: 1,
-        name: "sneha",
-        age: 21,
-        stream: "computer science",
-        college: "camellia institute of technology",
-      },
+      id: 1,
+      name: "sneha",
+      age: 21,
+      stream: "computer science",
+      college: "camellia institute of technology",
+    },
 
-      {
-        id: 1,
-        name: "sneha",
-        age: 21,
-        stream: "computer science",
-        college: "camellia institute of technology",
-      },
+    {
+      id: 1,
+      name: "sneha",
+      age: 21,
+      stream: "computer science",
+      college: "camellia institute of technology",
+    },
 
-      {
-        id: 1,
-        name: "sneha",
-        age: 21,
-        stream: "computer science",
-        college: "camellia institute of technology",
-      }
+    {
+      id: 1,
+      name: "sneha",
+      age: 21,
+      stream: "computer science",
+      college: "camellia institute of technology",
+    },
   ]);
 });
 
-
 app.get("/temp", (req, res) => {
-    res.status(200).json([
-      {
-        id: 1,
-        name: "sneha",
-        age: 21,
-        stream: "computer science",
-        college: "camellia institute of technology",
-      },
-  
-      {
-          id: 1,
-          name: "sneha",
-          age: 21,
-          stream: "computer science",
-          college: "camellia institute of technology",
-        },
-  
-        {
-          id: 1,
-          name: "sneha",
-          age: 21,
-          stream: "computer science",
-          college: "camellia institute of technology",
-        },
-  
-        {
-          id: 1,
-          name: "sneha",
-          age: 21,
-          stream: "computer science",
-          college: "camellia institute of technology",
-        }
-    ]);
-  });
+  res.status(200).json([
+    {
+      id: 1,
+      name: "sneha",
+      age: 21,
+      stream: "computer science",
+      college: "camellia institute of technology",
+    },
 
+    {
+      id: 1,
+      name: "sneha",
+      age: 21,
+      stream: "computer science",
+      college: "camellia institute of technology",
+    },
 
-  //the method are identical when an object or array is passed, but res.json() will also convert non object, such as null and undefined which are not valid JSON.
+    {
+      id: 1,
+      name: "sneha",
+      age: 21,
+      stream: "computer science",
+      college: "camellia institute of technology",
+    },
+
+    {
+      id: 1,
+      name: "sneha",
+      age: 21,
+      stream: "computer science",
+      college: "camellia institute of technology",
+    },
+  ]);
+});
+
+//the method are identical when an object or array is passed, but res.json() will also convert non object, such as null and undefined which are not valid JSON.
 
 app.listen(port, () => {
   console.log(`listen the port ${port}`);
